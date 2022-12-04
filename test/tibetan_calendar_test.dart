5733fda966d014bea81dbee4e596bf9b5d8f9ad4
codingwithtashi <khakontas33@gmail.com>
2022-12-04 18:48:42 +0530

    added test cases

--- test/tibetan_calendar_test.dart
+++ test/tibetan_calendar_test.dart
@@ -1,12 +1,23 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:tibetan_calendar/tibetan_calendar.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = TibetanCalendar();
-    /* expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);*/
+  test('Check Tibetan date by western date (2022, 11, 7)', () {
+    final tibetanDate = TibetanCalendar.getTibetanDate(DateTime(2022, 11, 7));
+    expect(tibetanDate.day, 14);
+    expect(tibetanDate.month, 9);
+    expect(tibetanDate.year, 2149);
+  });
+  test('Check Tibetan date by western date (2021, 12, 12)', () {
+    final tibetanDate = TibetanCalendar.getTibetanDate(DateTime(2021, 12, 12));
+    expect(tibetanDate.day, 9);
+    expect(tibetanDate.month, 10);
+    expect(tibetanDate.year, 2148);
+  });
+  test('Check Tibetan date by western date (2020, 10, 10)', () {
+    final tibetanDate = TibetanCalendar.getTibetanDate(DateTime(2020, 10, 10));
+    expect(tibetanDate.day, 23);
+    expect(tibetanDate.month, 8);
+    expect(tibetanDate.year, 2147);
   });
 }
